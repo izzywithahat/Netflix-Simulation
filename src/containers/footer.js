70ca12bdb25e95f8a0e182132af705907ef633eb
Izzy Russo <izzyr25@yahoo.com>
2023-10-21 15:54:37 -0600

    Created the footer.

---
+++ src/containers/footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Footer} from '../components';
+
+export function FooterContainer() {
+    return(
+        <Footer>
+            <Footer.Title>Questions? Contact us.</Footer.Title>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href="#">FAQ</Footer.Link>
+                    <Footer.Link href="#">Investor Rrelations</Footer.Link>
+                    <Footer.Link href="#">Buy Gift Card</Footer.Link>
+                    <Footer.Link href="#">Cookie Preferences</Footer.Link>
+                    <Footer.Link href="#">Legal Notices</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="#">Help Center</Footer.Link>
+                    <Footer.Link href="#">Jobs</Footer.Link>
+                    <Footer.Link href="#">Ways to Watch</Footer.Link>
+                    <Footer.Link href="#">Coporate Information</Footer.Link>
+                    <Footer.Link href="#">Only on Netflix</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="#">Account</Footer.Link>
+                    <Footer.Link href="#">Netflix Shop</Footer.Link>
+                    <Footer.Link href="#">Terms of Use</Footer.Link>
+                    <Footer.Link href="#">Contact Us</Footer.Link>
+                    <Footer.Link href="#">Do Not Sell or Share My Personal Information</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="#">Media Center</Footer.Link>
+                    <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
+                    <Footer.Link href="#">Privacy</Footer.Link>
+                    <Footer.Link href="#">Speed Test</Footer.Link>
+                    <Footer.Link href="#">Ad Choices</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.Break/>
+            <Footer.Text>Netflix United States</Footer.Text>
+        </Footer>
+    )
+}
