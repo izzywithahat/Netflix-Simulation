7c44b0c794e80cbecb55b6845c9a74e77734a870
Izzy Russo <izzyr25@yahoo.com>
2023-10-21 18:18:31 -0600

    Added the FAQ section on the home page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { FooterContainer } from "./containers/footer";
-import {JumbotronContainer} from './containers/jumbotron';
+import { JumbotronContainer } from "./containers/jumbotron";
+import { FaqsContainer } from "./containers/faqs";
 
 export default function App() {
   return (
-    <><JumbotronContainer/><FooterContainer />
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
     </>
-      
   );
 }
